afc095b9d8b2e662a9155bec2b05601a629ef325
OneNoth1ng <3602869979@qq.com>
2022-08-08 14:40:40 +0800

    修改了启动类的输出，增加辨识度

--- src/main/java/com/hcb/ReggieApplication.java
+++ src/main/java/com/hcb/ReggieApplication.java
@@ -16,7 +16,7 @@ public class ReggieApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class, args);
-        log.info("项目启动成功！");
+        log.info("V1.0项目启动成功！，此分支包含Redis缓存升级");
     }
 
 }
